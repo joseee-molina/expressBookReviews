34de413ec168700cd2c452633597161f9d1a157f
Jose Molina <77258399+joseee-molina@users.noreply.github.com>
2024-06-16 16:23:55 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,22 +6,50 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+for(user in users){
+    if(username == user.username){
+        return true;
+    }
+}
+return false;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password, res)=>{ //returns boolean
+    //write code to check if username and password match the one we have in records.
+    if(isValid(username)==false){
+return res.status(404).json({message: "Username invalid"});    }
+
+        for(user in users){
+            if((username == user.username) && (password == user.password)){
+                res.send("Authenticated!")
+                let accessToken = jwt.sign({
+                data: user
+                }, 'access', { expiresIn: 60 * 60 });
+                req.session.authorization = {
+                    accessToken
+                }
+                return res.status(200).send("User successfully logged in");
+            }
+
+        }
+    
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username
+  let password = req.body.password
+
+  return authenticatedUser(username, password, res);
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
+  let isbn = req.params.isbn
+  let username = req.session.username;
+  res.send(username)
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
